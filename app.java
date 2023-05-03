9f3378b42104dc210a395ea78b05cccf3abfa867
EC2 Default User <ec2-user@ip-172-31-24-157.eu-west-1.compute.internal>
2023-05-03 18:32:50 +0000

    my first commit

---
+++ app.java
@@ -0,0 +1,6 @@
+  # This code is a web application
+  # Please do not hard code
+  # Make use of standard  and dynamic variables
+  # This is my first file oooh
+  # I hope to get hired and ready to go
+  # Author: Sudobarn Technologies
